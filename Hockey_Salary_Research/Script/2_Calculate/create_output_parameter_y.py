2d8e2ae8a4b77c0c4e61a7882f46a957833363d4
MichaelKuby <105996614+MichaelKuby@users.noreply.github.com>
2024-03-22 15:54:11 -0700

    Y's correction
    Changed the calculation for our y's. Previously 2008 == 2008-09. Now 2008 == 2007-08 (for all years).

--- Hockey_Salary_Research/Script/2_Calculate/create_output_parameter_y.py
+++ Hockey_Salary_Research/Script/2_Calculate/create_output_parameter_y.py
@@ -11,7 +11,7 @@ import create_local_salary_dataframe
 
 
 def fetch_salary_cap_csv_path():
-    return '../../Data/Salary/SALARY_CAP.csv'
+    return '../../Data/Salary/SALARY_CAP.csv' # not the problem
 
 
 def convert_currency_to_int(currency_str):
@@ -19,28 +19,34 @@ def convert_currency_to_int(currency_str):
     return int(numeric_str)
 
 
-def fetch_lower_salary_cap(season):
+def fetch_upper_salary_cap(season):
     df_salary_cap = pd.read_csv(fetch_salary_cap_csv_path())
 
-    season_str = str(season)
-    next_season_suffix = str(season + 1)[-2:]
-    key_season = f"{season_str}-{next_season_suffix}"
+    season_str_suffix = str(season)[2:]
+    # Get the previous season's suffix
+    prev_season = str(season -1)
+    key_season = f"{prev_season}-{season_str_suffix}"
 
     matching_row = df_salary_cap[df_salary_cap['SEASON'] == key_season]
     if not matching_row.empty:
-        return convert_currency_to_int(matching_row['LOWER LIMIT'].iloc[0])
+        return convert_currency_to_int(matching_row['UPPER LIMIT\n'].iloc[0])
     else:
         return None
 
 
 def main():
+    
+    # Set the current working directory to the script's directory
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
     for season in range(2008, 2025):
         csv_file_path = create_local_salary_dataframe.fetch_local_csv_path(season)
         df_season = pd.read_csv(csv_file_path)
-        df_season['season'] = str(season)
+        df_season['season'] = str(season-1) + '-' + str(season)[2:]
 
-        lower_salary_cap = fetch_lower_salary_cap(season)
-        df_season['Y_Salary_Cap_Percentage'] = df_season['CAP HIT'].apply(convert_currency_to_int) / lower_salary_cap
+        upper_salary_cap = fetch_upper_salary_cap(season)
+        df_season['Y_Salary_Cap'] = upper_salary_cap
+        df_season['Y_Salary_Cap_Percentage'] = df_season['CAP HIT'].apply(convert_currency_to_int) / upper_salary_cap
 
         df_season.to_csv(create_local_salary_dataframe.fetch_local_csv_path(season), index=False, encoding='utf-8')
 
